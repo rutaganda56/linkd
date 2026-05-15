2cb8103c172658f2af610129607159a58bd592f9
rutaganda56 <valentinrutaganda04@gmail.com>
2026-05-15 20:55:20 +0300

    modified the setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="linkd",
     version="1.0.1",
+    author="Rutaganda Jean Valentin",
     py_modules=["main", "github_api", "reddit_api", "utils"], 
     packages=find_packages(),
+    install_requires=[
+        "rich",
+        "requests"
+    ],
+    include_package_data=True,
     entry_points={
         "console_scripts": [
-            "linkd=main:main"
+            "linkd=linkd.main:main"
         ]
     },
 )
--- src/config.py
+++
@@ -1,5 +0,0 @@
-APP_NAME = "LINKD CLI"
-
-VERSION = "1.0.0"
-
-AUTHOR = "Rutaganda Valentin"
